ed82d26ec0c7f36548ca78d751d16969958ac83f
Srikanth Chebrolu <srikanth.chebrolu@rackspace.com>
2021-09-16 16:46:07 +0530

    [Srikanth Chebrolu] Initial Commit

---
+++ src/app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+var app = express();
+const port = 5000;
+
+var router = express.Router();
+
+app.use('/', router);
+
+router.use(compression());
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+
+//Swagger Configuration
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Node Sample Project',
+      version: '1.0.0',
+      description:
+        'This Project gives a Basic Idea of the Node Backed with Express and Test with Mocha',
+      contact: { email: 'srikanthchebrolu3333@gmail.com' },
+      servers: ['http://localhost:' + port]
+    }
+  },
+  swagger: '2.0',
+  openapi: '3.0.0',
+  basePath: '/test',
+  schemes: ['http', 'https'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  apis: ['app.js']
+};
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve);
+app.get('/api-docs', swaggerUI.setup(swaggerDocs, { explorer: true }));
+
+app.get('/test/us', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/test/users', (req, res, next) => {
+  res.send(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food']);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = app;
---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+var assert = require('mocha');
+
+describe('my suite', () => {
+  it('sample test', () => {
+    // should set the timeout of this test to 1000 ms; instead will fail
+  });
+});
